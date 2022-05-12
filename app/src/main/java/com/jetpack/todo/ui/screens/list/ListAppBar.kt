511fae05f566e88f60ed6a64148212e4ed6b168e
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2022-05-12 19:59:22 +0530

    Seach bar Added

--- app/src/main/java/com/jetpack/todo/ui/screens/list/ListAppBar.kt
+++ app/src/main/java/com/jetpack/todo/ui/screens/list/ListAppBar.kt
@@ -1,7 +1,13 @@
 package com.jetpack.todo.ui.screens.list
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -15,15 +21,28 @@ import com.jetpack.todo.ui.theme.topAppBarBackgroundColor
 import com.jetpack.todo.ui.theme.topAppbarContentColor
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import com.jetpack.todo.componants.PriorityItem
+import com.jetpack.todo.ui.theme.TOP_APP_BAR_HEIGHT
 
 @Composable
 fun ListAppBar(
 ){
-    DefaultListAppBar(
-        onSearchClicked ={},
-        onSortClicked = {},
-        onDeleteClicked = {}
+//    DefaultListAppBar(
+//        onSearchClicked ={},
+//        onSortClicked = {},
+//        onDeleteClicked = {}
+//    )
+    SearchAppBar(
+        text = "",
+        onTextChange = {},
+        onCloseClicked = {},
+        onSearchClicked = {}
     )
 }
 
@@ -135,11 +154,90 @@ fun DeleteAction(
 }
 
 @Composable
+fun SearchAppBar(
+    text : String,
+    onTextChange :(String) -> Unit,
+    onCloseClicked : () -> Unit,
+    onSearchClicked: (String) -> Unit
+){
+    Surface(modifier = Modifier
+        .fillMaxWidth()
+        .height(TOP_APP_BAR_HEIGHT),
+        elevation = AppBarDefaults.TopAppBarElevation,
+        color = MaterialTheme.colors.topAppBarBackgroundColor
+    ) {
+        TextField(
+            value = text,
+            modifier = Modifier.fillMaxWidth(),
+            onValueChange = {
+                onTextChange(it)
+            },
+            placeholder =  {
+                Text(
+                    modifier = Modifier.alpha(ContentAlpha.medium),
+                    text = "Search",
+                color = Color.White)
+        },
+            textStyle = TextStyle(
+                color = MaterialTheme.colors.topAppbarContentColor,
+                fontSize = MaterialTheme.typography.subtitle1.fontSize
+            ),
+            singleLine = true,
+            leadingIcon = {
+                IconButton(
+                    modifier = Modifier.alpha(ContentAlpha.disabled),
+                    onClick = { }) {
+                    Icon(
+                        imageVector = Icons.Filled.Search,
+                    contentDescription = "Search Icon",
+                    tint = MaterialTheme.colors.topAppbarContentColor)
+
+                }
+            },
+            trailingIcon = {
+                IconButton(onClick = {onCloseClicked()}) {
+                    Icon(imageVector = Icons.Filled.Close,
+                    contentDescription = "Close icon",
+                    tint = MaterialTheme.colors.topAppbarContentColor)
+                }
+            },
+            keyboardOptions = KeyboardOptions(
+                imeAction = ImeAction.Search
+            ),
+            keyboardActions = KeyboardActions(
+                onSearch = {
+                    onSearchClicked(text)
+                }
+            ),
+            colors = TextFieldDefaults.textFieldColors(
+                cursorColor = MaterialTheme.colors.topAppbarContentColor,
+                focusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                backgroundColor = Color.Transparent
+            )
+        )
+    }
+}
+
+
+@Composable
 @Preview
 private fun DefaultListAppBarPreview(){
     DefaultListAppBar(
         onSearchClicked = {},
         onSortClicked = {},
         onDeleteClicked = {}
+    )
+}
+
+@Composable
+@Preview
+private fun SearchAppbarPreview() {
+    SearchAppBar(
+        text = "",
+        onTextChange = {},
+        onCloseClicked = {},
+        onSearchClicked = {}
     )
 }
