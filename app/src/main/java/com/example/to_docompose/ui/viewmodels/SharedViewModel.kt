19bb9512ec065b64d2279a9be85e8833e48c602a
Pushpak Gosavi <pushpak.gosavi7@gmail.com>
2022-06-16 19:59:36 +0530

    data store added

--- app/src/main/java/com/example/to_docompose/ui/viewmodels/SharedViewModel.kt
+++ app/src/main/java/com/example/to_docompose/ui/viewmodels/SharedViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.to_docompose.data.models.Priority
 import com.example.to_docompose.data.models.ToDoTask
+import com.example.to_docompose.data.repositories.DataStoreRepository
 import com.example.to_docompose.data.repositories.ToDoRepository
 import com.example.to_docompose.util.Action
 import com.example.to_docompose.util.Constants.MAX_TITLE_LENGTH
@@ -22,7 +23,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class SharedViewModel @Inject constructor(
-    private val repository: ToDoRepository
+    private val repository: ToDoRepository,
+    private val dataStoreRepository: DataStoreRepository
 ) : ViewModel() {
     val action:MutableState<Action> = mutableStateOf(Action.NO_ACTION)
 
