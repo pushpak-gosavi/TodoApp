e56c146197e02cdc724960de4802d7ac34049c2f
Pushpak <pushpak.gosavi7@gmail.com>
2023-08-03 11:19:45 +0530

    package_update:unstable- In App update unctioanlity used

--- app/src/main/java/com/jetpack/to_docompose/MainActivity.kt
+++ app/src/main/java/com/jetpack/to_docompose/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.jetpack.to_docompose
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
@@ -11,6 +15,10 @@ import com.jetpack.to_docompose.navigation.SetupNavigation
 import com.jetpack.to_docompose.ui.theme.ToDoComposeTheme
 import com.jetpack.to_docompose.ui.viewmodels.SharedViewModel
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
+import com.google.android.play.core.appupdate.AppUpdateManagerFactory
+import com.google.android.play.core.appupdate.AppUpdateOptions
+import com.google.android.play.core.install.model.AppUpdateType
+import com.google.android.play.core.install.model.UpdateAvailability
 import dagger.hilt.android.AndroidEntryPoint
 
 @ExperimentalAnimationApi
@@ -32,5 +40,44 @@ class MainActivity : ComponentActivity() {
                 )
             }
         }
+        checkAppUpdate()
     }
+
+    private fun checkAppUpdate() {
+        val appUpdateManager = AppUpdateManagerFactory.create(this)
+
+// Returns an intent object that you use to check for an update.
+        val appUpdateInfoTask = appUpdateManager.appUpdateInfo
+
+// Checks that the platform will allow the specified type of update.
+        appUpdateInfoTask.addOnSuccessListener { appUpdateInfo ->
+            if (appUpdateInfo.updateAvailability() == UpdateAvailability.UPDATE_AVAILABLE
+                // This example applies an immediate update. To apply a flexible update
+                // instead, pass in AppUpdateType.FLEXIBLE
+                && appUpdateInfo.isUpdateTypeAllowed(AppUpdateType.IMMEDIATE)
+            ) {
+                appUpdateManager.startUpdateFlowForResult(
+                    // Pass the intent that is returned by 'getAppUpdateInfo()'.
+                    appUpdateInfo,
+                    // an activity result launcher registered via registerForActivityResult
+                    activityResultLauncher,
+                    // Or pass 'AppUpdateType.FLEXIBLE' to newBuilder() for
+                    // flexible updates.
+                    AppUpdateOptions.newBuilder(AppUpdateType.IMMEDIATE).build()
+                )
+            }
+        }
+    }
+
+    var activityResultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { result: ActivityResult ->
+            {
+                // handle callback
+                if (result.resultCode != RESULT_OK) {
+                    Log.w("", "Update flow failed! Result code: " + result.resultCode);
+                    // If the update is cancelled or fails,
+                    // you can request to start the update again.
+                }
+            }
+        }
 }
